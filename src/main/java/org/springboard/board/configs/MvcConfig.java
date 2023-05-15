d1a9ece249188dcdce05d55b166d2ab33e27a966
lww9562 <lww9562@naver.com>
2023-05-15 12:51:02 +0900

    템플릿 레이아웃 프론트 완료

--- src/main/java/org/springboard/board/configs/MvcConfig.java
+++ src/main/java/org/springboard/board/configs/MvcConfig.java
@@ -1,13 +1,20 @@
 package org.springboard.board.configs;
 
+import lombok.RequiredArgsConstructor;
+import org.springboard.board.configs.interceptors.SiteConfigInterceptor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@RequiredArgsConstructor
 public class MvcConfig implements WebMvcConfigurer {
+	// 사이트 설정 유지 인터셉터
+	private final SiteConfigInterceptor siteConfigInterceptor;
+
 	@Value("${file.upload.path}")
 	private String fileUploadPath;
 
@@ -21,5 +28,11 @@ public class MvcConfig implements WebMvcConfigurer {
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		registry.addResourceHandler("/uploads/**")
 				.addResourceLocations("file:///" + fileUploadPath);
+	}
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(siteConfigInterceptor)
+				.addPathPatterns("/**");
 	}
 }
