579ffe4cc0b7a7a0a2d3ca819943d290933bd275
lww9562 <lww9562@naver.com>
2023-05-17 10:52:12 +0900

    스프링 시큐리티 LoginFailureHandler, LoginSuccessHandler 작업 완료

--- src/main/java/org/springboard/board/configs/SecurityConfig.java
+++ src/main/java/org/springboard/board/configs/SecurityConfig.java
@@ -1,5 +1,7 @@
 package org.springboard.board.configs;
 
+import org.springboard.board.models.member.LoginFailureHandler;
+import org.springboard.board.models.member.LoginSuccessHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -17,9 +19,8 @@ public class SecurityConfig {
 				.loginPage("/member/login")
 				.usernameParameter("userId")
 				.passwordParameter("userPw")
-				.defaultSuccessUrl("/")
-				.failureForwardUrl("/member/login")
-				.loginProcessingUrl("/member/login")
+				.successHandler(new LoginSuccessHandler())
+				.failureHandler(new LoginFailureHandler())
 				.and()
 				.logout()
 				.logoutRequestMatcher(new AntPathRequestMatcher("/member/logout"))
