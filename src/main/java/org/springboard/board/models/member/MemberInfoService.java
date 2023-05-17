0b22a3b38fa4f83a9a0fc43e67bf7c1129ee4a8c
lww9562 <lww9562@naver.com>
2023-05-17 09:51:37 +0900

    스프링 시큐리티 로그인 설정

---
+++ src/main/java/org/springboard/board/models/member/MemberInfoService.java
@@ -0,0 +1,32 @@
+package org.springboard.board.models.member;
+
+import lombok.RequiredArgsConstructor;
+import org.springboard.board.entities.Member;
+import org.springboard.board.repositories.MemberRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MemberInfoService implements UserDetailsService {
+	private final MemberRepository repository;
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		Member member = repository.findByUserId(username);
+		if(member == null){
+			throw new UsernameNotFoundException(username);
+		}
+
+		return MemberInfo.builder()
+				.userNo(member.getUserNo())
+				.userId(member.getUserId())
+				.userPw(member.getUserPw())
+				.userNm(member.getUserNm())
+				.email(member.getEmail())
+				.mobile(member.getMobile())
+				.build();
+	}
+}
