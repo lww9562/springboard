fae3ec41ed2d94a42b5651b48f14e3a220d88c63
lww9562 <lww9562@naver.com>
2023-05-18 09:30:55 +0900

    공통 예외 처리 완료

---
+++ src/main/java/org/springboard/board/restcontrollers/FileUploadController.java
@@ -0,0 +1,12 @@
+package org.springboard.board.restcontrollers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FileUploadController {
+	@GetMapping("/file/upload")
+	public void upload() {
+
+	}
+}
