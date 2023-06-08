df01b6c2b88ac6d459aff500e04faa33c263a3d5
lww9562 <lww9562@naver.com>
2023-06-08 11:07:07 +0900

    파일 업로드 구현 중

---
+++ src/main/java/org/springboard/board/models/file/FileNotFoundException.java
@@ -0,0 +1,10 @@
+package org.springboard.board.models.file;
+
+import org.springboard.board.commons.CommonException;
+import org.springframework.http.HttpStatus;
+
+public class FileNotFoundException extends CommonException {
+	public FileNotFoundException() {
+		super(bundleError.getString("File.notExists"), HttpStatus.BAD_REQUEST);
+	}
+}
---
+++ src/main/java/org/springboard/board/repositories/FileInfoRepository.java
@@ -0,0 +1,9 @@
+package org.springboard.board.repositories;
+
+import org.springboard.board.entities.FileInfo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+
+public interface FileInfoRepository extends JpaRepository<FileInfo, Long>, QuerydslPredicateExecutor<FileInfo> {
+
+}
